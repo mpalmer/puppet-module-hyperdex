a850090a05f3337cae24b548501a4bc0fbe2ffaa
Matt Palmer <mpalmer@hezmatt.org>
2014-09-02 15:41:44 +1000

    Switch to managing base_directory, not data_directory

--- manifests/coordinator.pp
+++ manifests/coordinator.pp
@@ -27,53 +27,44 @@
 #
 #     The port to use to talk to another coordinator.
 #
-#  * `data_directory` (string; optional; default `/srv/hyperdex/$name/data`)
+#  * `base_directory` (string; optional; default `/srv/hyperdex/$name`)
 #
-#     Where to store the database files that the coordinator uses.  This
-#     resource will automatically create the directory you specify, but not
-#     any parents of it (with the exception of `/srv/hyperdex/$name`, if you
-#     stick with the default).
+#     Where the hyperdex coordinator will store all its data.  This resource
+#     will automatically create the directory you specify, but not any
+#     parents of it.
 #
 define hyperdex::coordinator(
 		$listen_address      = undef,
 		$listen_port         = 1982,
 		$coordinator_address,
 		$coordinator_port    = 1982,
-		$data_directory      = undef,
+		$base_directory      = undef,
 ) {
 	include hyperdex::user
 	include hyperdex::packages::server
 
-	if $data_directory == undef {
-		$_data_directory = "/srv/hyperdex/${name}/data"
-		file { "/srv/hyperdex/${name}":
-			ensure => directory,
-			mode   => 0755,
-			owner  => "root",
-			group  => "root",
-		}
+	if $base_directory == undef {
+		$_base_directory = "/srv/hyperdex/${name}"
 	} else {
-		$_data_directory = $data_directory
+		$_base_directory = $base_directory
 	}
 
-	file { $_data_directory:
-		ensure  => directory,
-		mode    => 0755,
-		owner   => "hyperdex",
-		group   => "hyperdex",
-		require => Class["hyperdex::packages::server"],
+	file { [$_base_directory, "${_base_directory}/data"]:
+		ensure => directory,
+		mode   => 0755,
+		owner  => "hyperdex",
+		group  => "hyperdex",
 	}
-
 	if $listen_address {
 		$listen_addr_opt = " -l ${listen_address}"
 	}
 
 	daemontools::service { $name:
-		command => "/usr/bin/hyperdex coordinator -f -D ${_data_directory}${listen_addr_opt} -p ${listen_port} -c ${coordinator_address} -P ${coordinator_port}",
+		command => "/usr/bin/hyperdex coordinator -f -D ${_base_directory}/data${listen_addr_opt} -p ${listen_port} -c ${coordinator_address} -P ${coordinator_port}",
 		user    => "hyperdex",
 		require => [
 			Class["hyperdex::packages::server"],
-			File[$_data_directory],
+			File["${_base_directory}/data"],
 		],
 	}
 }
--- manifests/daemon.pp
+++ manifests/daemon.pp
@@ -29,41 +29,33 @@
 #
 #     The port to use to talk to the coordinator.
 #
-#  * `data_directory` (string; optional; default `/srv/hyperdex/$name/data`)
+#  * `base_directory` (string; optional; default `/srv/hyperdex/$name`)
 #
-#     Where to store the database files that the data daemon uses.  This
-#     resource will automatically create the directory you specify, but not
-#     any parents of it (with the exception of `/srv/hyperdex/$name`, if you
-#     stick with the default).
+#     Where the data daemon will store all its data.  This resource will
+#     automatically create the directory you specify, but not any parents of
+#     it.
 #
 define hyperdex::daemon(
 		$listen_address      = undef,
 		$listen_port         = 2012,
 		$coordinator_address = "localhost",
 		$coordinator_port    = 1982,
-		$data_directory      = undef,
+		$base_directory      = undef,
 ) {
 	include hyperdex::user
 	include hyperdex::packages::server
 
-	if $data_directory == undef {
-		$_data_directory = "/srv/hyperdex/${name}/data"
-		file { "/srv/hyperdex/${name}":
-			ensure => directory,
-			mode   => 0755,
-			owner  => "root",
-			group  => "root",
-		}
+	if $base_directory == undef {
+		$_base_directory = "/srv/hyperdex/${name}"
 	} else {
-		$_data_directory = $data_directory
+		$_base_directory = $base_directory
 	}
 
-	file { $_data_directory:
+	file { [$_base_directory, "${_base_directory}/data"]:
 		ensure  => directory,
 		mode    => 0755,
 		owner   => "hyperdex",
 		group   => "hyperdex",
-		require => Class["hyperdex::packages::server"],
 	}
 
 	if $listen_address {
@@ -71,11 +63,11 @@ define hyperdex::daemon(
 	}
 
 	daemontools::service { $name:
-		command => "/usr/bin/hyperdex daemon -f -D ${_data_directory}${listen_addr_opt} -p ${listen_port} -c ${coordinator_address} -P ${coordinator_port}",
+		command => "/usr/bin/hyperdex daemon -f -D ${_base_directory}/data${listen_addr_opt} -p ${listen_port} -c ${coordinator_address} -P ${coordinator_port}",
 		user    => "hyperdex",
 		require => [
 			Class["hyperdex::packages::server"],
-			File[$_data_directory],
+			File["${_base_directory}/data"],
 		],
 	}
 }
